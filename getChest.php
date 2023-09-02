5d745b755d9e5ebed94ef36f9bf1e6554d459a22
ApeBlin <arttu.raittinen@hotmail.fi>
2023-09-02 10:40:22 +0300

    added database data. added test img NEED TESTING

--- getChest.php
+++ getChest.php
@@ -5,7 +5,7 @@ if(isset($_GET['chest_id']) && is_numeric($_GET['chest_id']))
 {
 	$userID = intval($_GET['chest_id']);
 	
-	$qry = "select name, layer from Chest where id = ".$userID;
+	$qry = "select name, layer, img from Chest where id = ".$userID;
 
 	$rs = $dbConn->query($qry);
 
@@ -13,13 +13,19 @@ if(isset($_GET['chest_id']) && is_numeric($_GET['chest_id']))
 	foreach ($fetchChestData as $chestData) {
 		echo $chestData['name'];
 	}
-
-
-
 	$rs->close();
-
 	$dbConn->close();
 
-		
+	// Use this code to display the image and also use the layer data on the z-index. !!NOT TESTED!!
+	// $fetchChestData = $rs->fetch_all(MYSQLI_ASSOC);
+    // foreach ($fetchChestData as $chestData) {
+    //     $imgUrl = $chestData['img'];
+    //     $layer = $chestData['layer'];
+
+    //     // Use inline CSS to set the z-index property based on the layer data
+    //     echo '<img src="' . $imgUrl . '" alt="" style="z-index: ' . $layer . ';">';
+    // }
+    // $rs->close();
+    // $dbConn->close();	
 }
 ?>
